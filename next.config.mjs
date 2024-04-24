048d320a384eb212cc7d692d79e999b3d6ac0032
alfat <gilmalf89@gmail.com>
2024-04-24 16:34:08 +0500

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: '/s.stocks.soccer',
   reactStrictMode: true,
   webpack: (config) => {
     config.module.rules.push({
