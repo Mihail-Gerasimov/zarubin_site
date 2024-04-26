4d1c4237c32a3aaa006f23da122f402c2df5cd0b
ALFAT GILMANSHIN <89654590+Alfat1989@users.noreply.github.com>
2024-04-26 11:48:25 +0500

    feat: add bg image (#19)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Header = ({ children }: { children: React.ReactNode }) => {
   return (
-    <header className='px-[24px] py-[40px] desktop:px-[55px] backdrop-blur-md sticky top-0 bg-purple-main/30 bg-opacity-5'>
+    <header className='px-[24px] py-[40px] desktop:px-[55px] backdrop-blur-md sticky top-0 z-10 bg-purple-main/30 bg-opacity-5'>
       <nav className='w-full'>{children}</nav>
     </header>
   );
