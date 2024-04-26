906e5fb355bb2a0ffdb56b258ab4223e822ca5ef
alfat <gilmalf89@gmail.com>
2024-04-26 19:14:45 +0500

    fix: add link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import LightMainLogo from '@/public/assets/images/icons/second_logo.svg';
 
 export const Footer = () => {
   return (
-    <footer className='fixed bottom-0 w-full h-[78px] p-[0_24px] bg-black-bg desktop:p-[0_144px] flex justify-between text-[14px] leading-[1.2] desktop:text-[18px] desktop:leading-[1.1] items-center desktop:h-[82px]'>
-      <div className='mx-[auto] max-w-[896px] w-full flex justify-between'>
+    <footer className='fixed bottom-0 w-full py-[24px] bg-[#222222] flex justify-between text-[14px] leading-[1.2] desktop:text-[18px] desktop:leading-[1.1] items-center'>
+      {/* <div className='mx-[auto] max-w-[896px] w-full flex justify-between'>
         <span className='text-red'>&#169; 2024</span>
         <Link
           href='https://zarubin.co.uk/policy_en'
@@ -12,6 +13,21 @@ export const Footer = () => {
         >
           Policy Privacy
         </Link>
+      </div> */}
+      <div className='mx-[auto] w-[100%] max-w-[897px] flex flex-col items-start'>
+        <ul className='flex flex-col space-y-4'>
+          <li>
+            <a href='https://stocks.soccer/'>Home</a>
+          </li>
+          <li>
+            <a href='https://stocks.soccer/'>to Leagues</a>
+          </li>
+          <li>
+            <a href='#'>
+              <LightMainLogo className='w-[auto] h-[36px]' />
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
