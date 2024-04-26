66031a075b6b3366bf328fe83674e3a82eb49005
alfat <gilmalf89@gmail.com>
2024-04-26 22:10:54 +0500

    fix: add footer logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import LightMainLogo from '@/public/assets/images/icons/second_logo.svg';
 
 export const Footer = () => {
   return (
-    <footer className='fixed bottom-0 w-full py-[24px] bg-[#222222] flex justify-between text-[14px] leading-[1.2] desktop:text-[18px] desktop:leading-[1.1] items-center'>
-      <div className='mx-[auto] w-[100%] max-w-[897px] flex flex-rows justify-between items-center'>
+    <footer className='fixed bottom-0 w-full px-[24px] py-[24px] bg-[#222222] flex justify-between text-[14px] leading-[1.2] desktop:text-[18px] desktop:leading-[1.1] items-center'>
+      <div className='mx-[auto] w-[100%] max-w-[897px] flex flex-col justify-between items-start gap-[12px] tablet:flex-row tablet:items-center'>
         <ul className='flex flex-col space-y-4'>
           <li>
             <a href='https://stocks.soccer/'>Home</a>
@@ -12,8 +12,12 @@ export const Footer = () => {
             <a href='https://stocks.soccer/'>to Leagues</a>
           </li>
         </ul>
-        <div className='flex items-center justify-center'>
-          <a href='#' className='flex items-center gap-[12px]'>
+        <div className='mx-[auto] tablet:mx-0 flex items-center justify-center'>
+          <a
+            href='https://zarubin.co.uk/'
+            className='flex items-center gap-[12px]'
+            target='_blank'
+          >
             powered by @
             <LightMainLogo className='w-[auto] h-[36px]' />
           </a>
