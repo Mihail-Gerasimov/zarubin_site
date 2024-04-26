cf037d741e2182b32e33a0a7b15c57b66c9fab40
ALFAT GILMANSHIN <89654590+Alfat1989@users.noreply.github.com>
2024-04-26 09:51:29 +0500

    fix: add sort with date (#18)

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,18 @@ import { BlogCard } from '@/src/components/BlogCard/BlogCard';
 import { Header } from '@/src/components/Header/Header';
 import { MainList } from '@/src/components/NavList/MainList';
 import { getPostMetadata } from '@/src/utils/getPostMetadata';
+import { DateTime } from 'luxon';
 
 export default function Home() {
   const postMetadata = getPostMetadata('src/posts');
 
-  // const sortedPosts = postMetadata.sort((a, b) => a.date - b.date);
+  const sortedPosts = postMetadata.sort((a, b) => {
+    const dateA = DateTime.fromFormat(a.date, 'dd-MM-yyyy');
+    const dateB = DateTime.fromFormat(b.date, 'dd-MM-yyyy');
+    if (dateA > dateB) return -1;
+    if (dateA < dateB) return 1;
+    return 0;
+  });
 
   return (
     <>
@@ -15,7 +22,7 @@ export default function Home() {
       </Header>
       <main className='px-[14px] py-[20px] pb-[78px] w-full'>
         <div className='mx-auto py-12 max-w-[896px] flex flex-col items-center gap-12'>
-          {postMetadata.reverse().map((post, idx) => (
+          {sortedPosts.map((post, idx) => (
             <BlogCard key={idx} post={post} />
           ))}
         </div>
