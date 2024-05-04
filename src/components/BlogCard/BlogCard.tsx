1d4cf2a3ed24706db4fd05cea3fe4e52d8c4f8fa
ALFAT GILMANSHIN <89654590+Alfat1989@users.noreply.github.com>
2024-05-04 20:19:42 +0500

    fix: post card style (#29)

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -19,25 +19,30 @@ export const BlogCard = ({ post }: Props) => {
     url = BASE_URL;
   }
 
+  const description =
+    post.description.length < 150
+      ? post.description
+      : post.description.slice(0, 150).trim() + '...';
+
   return (
     <Link
       href={`/${post.slug}`}
-      className='p-[10px] w-full flex rounded-lg ring-1 ring-inset ring-purple-light'
+      className='w-full flex rounded-[20px] bg-main-gray'
     >
       <div className='w-full'>
-        <div className='overflow-hidden'>
+        <div className='h-[128px] tablet:h-[196px] desktop:h-[241px] w-full relative overflow-hidden rounded-[20px]'>
           <Image
             src={`${url}${post.image}`}
             alt='image'
             width={300}
-            height={180}
-            className='w-full h-60 object-cover'
+            height={128}
+            className='top-0 left-0 w-full h-full object-cover absolute'
             unoptimized
           />
         </div>
-        <div className='p-[24px] flex flex-col items-start'>
-          <h3 className='mt-2 text-[20px]'>{post.title}</h3>
-          <p className='mt-3 text-[16px]'>{post.description}</p>
+        <div className='p-[14px_16px_16px] flex flex-col gap-[10px] items-start tablet:p-[40px_24px_24px] tablet:gap-[24px] desktop:p-[40px]'>
+          <h3 className='text-[16px] tablet:text-[32px]'>{post.title}</h3>
+          <p className='text-[14px] tablet:text-[21px]'>{description}</p>
         </div>
       </div>
     </Link>
