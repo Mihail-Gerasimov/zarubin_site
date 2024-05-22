dee072ac69c2e991ecf9bcf7ae96b9afad2faad0
alfat <gilmalf89@gmail.com>
2024-05-22 17:34:23 +0500

    fix: hide metrik

--- src/components/Metrika/YandexMetrika.tsx
+++ src/components/Metrika/YandexMetrika.tsx
@@ -2,15 +2,17 @@
 
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
-import ym from 'react-yandex-metrika';
+// import ym from 'react-yandex-metrika';
 
 export default function YandexMetrika() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    const url = `${pathname}?${searchParams}`;
-    ym('68177191', 'hit', url);
+    console.log(pathname);
+
+    // const url = `${pathname}?${searchParams}`;
+    // ym('68177191', 'hit', url);
   }, [pathname, searchParams]);
 
   return null;
