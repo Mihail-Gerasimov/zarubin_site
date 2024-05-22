1b4c2eb9c545817b1fc8604df90c173fc6b46d12
ALFAT GILMANSHIN <89654590+Alfat1989@users.noreply.github.com>
2024-05-23 01:48:37 +0500

    fix: add mobile menu (#13)

---
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,51 @@
+import TelegramIcon from '@/public/assets/images/icons/telegram.svg';
+import { Logo } from '@/src/ui-kit/LogoIcon/Logo';
+import { menuListLayer } from '@/src/utils/menuListLayer';
+import classNames from 'classnames';
+import Link from 'next/link';
+import styles from './MobileMenu.module.css';
+
+interface Props {
+  isOpen: boolean;
+}
+
+export const MobileMenu = ({ isOpen }: Props) => {
+  return (
+    <div className={classNames(styles.menuWeapper, isOpen && styles.active)}>
+      <Logo />
+      <div className={styles.content}>
+        <div className={styles.listWrapper}>
+          <ul className={styles.menuList}>
+            {menuListLayer.map((item) => (
+              <li key={item.id}>
+                <Link href={item.link} className={styles.menuLink}>
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.contactWrapper}>
+          <Link href='tel:+77777777777' className={styles.contactBtn}>
+            Contact us
+          </Link>
+          <div className='flex flex-col gap-[12px]'>
+            <Link
+              href='mailto:hello@digitalburo.tech'
+              className='text-[22px] text-main-black tablet:text-[32px]'
+            >
+              hello@digitalburo.tech
+            </Link>
+            <Link
+              href=''
+              className='flex items-center gap-[12px] text-[16px] text-main-black tablet:text-[26px]'
+            >
+              <TelegramIcon className='w-[30px] h-[auto]' />
+              Свяжитесь с нами в Telegram
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/utils/useMediaQuery.ts
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+
+type Conditions = string | string[];
+
+export const breakpoints = {
+  phone: 480,
+  tablet: 768,
+  desktop: 960,
+  'desktop-m': 1280,
+  'desktop-l': 1440,
+};
+
+const getOperator = (condition: string) =>
+  ['>=', '>', '<=', '<'].find((operator) =>
+    condition.startsWith(operator),
+  ) as string;
+
+const getPrefix = (operator: string) =>
+  ['<', '<='].includes(operator) ? 'max' : 'min';
+
+const getValue = (condition: string, operator: string) => {
+  const rawValue = condition.replace(operator, '');
+  let value =
+    breakpoints[rawValue as keyof typeof breakpoints] || Number(rawValue);
+  if (operator === '>') {
+    value += 1;
+  } else if (operator === '<') {
+    value -= 1;
+  }
+  return value;
+};
+
+const parse = (conditions: Conditions) =>
+  (Array.isArray(conditions) ? conditions : [conditions]).map((condition) => {
+    const operator = getOperator(condition);
+    const prefix = getPrefix(operator);
+    const value = getValue(condition, operator);
+    return `(${prefix}-width: ${value}px)`;
+  });
+
+const useMediaQuery = (
+  conditions: Conditions,
+  joinOperator: 'and' | 'or' = 'and',
+) => {
+  const [matches, setMatches] = useState<boolean>(false);
+
+  useEffect(() => {
+    const query = parse(conditions).join(` ${joinOperator} `);
+    const mkl = window.matchMedia(query);
+    setMatches(mkl.matches);
+    const updateMatches = (e: MediaQueryListEvent) => setMatches(e.matches);
+
+    mkl.addEventListener('change', updateMatches);
+
+    return () => mkl.removeEventListener('change', updateMatches);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [conditions.toString(), joinOperator]);
+
+  return matches;
+};
+
+export default useMediaQuery;
