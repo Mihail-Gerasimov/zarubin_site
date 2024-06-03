a62f11f177117cf8b3d54eb23cd4e2034059360e
alfat <gilmalf89@gmail.com>
2024-06-03 19:54:47 +0500

    fix: url path

--- src/ui-kit/LogoIcon/Logo.tsx
+++ src/ui-kit/LogoIcon/Logo.tsx
@@ -1,11 +1,11 @@
 import LogoIcon from '@/public/assets/images/icons/dark_logo.svg';
-import { BASE_URL } from '@/src/utils/alias';
+// import { BASE_URL } from '@/src/utils/alias';
 
-const URL = process.env.NODE_ENV === 'production' ? BASE_URL : '/';
+// const URL = process.env.NODE_ENV === 'production' ? BASE_URL : '/';
 
 export const Logo = () => {
   return (
-    <a href={URL}>
+    <a href='/'>
       <LogoIcon className='h-[40px] w-[auto] tablet:h-[62px]' />
     </a>
   );
