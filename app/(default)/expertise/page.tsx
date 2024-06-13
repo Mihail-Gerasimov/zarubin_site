f3d4b814a0c7dcaf4cf9b1cb449a06b45506d1cd
Arsalan Rabdanov <uzarsalan@gmail.com>
2024-06-13 10:54:32 +0300

    refactored expertise partially

--- app/(default)/expertise/page.tsx
+++ app/(default)/expertise/page.tsx
@@ -7,54 +7,68 @@ import { ProjectsRisks } from '@/src/components/Expertise/ProjectsRisks/Projects
 import { ScalableArchitecture } from '@/src/components/Expertise/ScalableArchitecture/ScalableArchitecture';
 import { SystemIntegration } from '@/src/components/Expertise/SystemIntegration/SystemIntegration';
 import { Virtual } from '@/src/components/Expertise/Virtual/Virtual';
-import { Footer } from '@/src/components/Footer/Footer';
-import { Header } from '@/src/components/Header/Header';
 import { ContactForm } from '@/src/components/Main/ContactForm/ContactForm';
 import { FeedbackClient } from '@/src/components/Main/Feedback/FeedbackClient';
 import { Insights } from '@/src/components/Main/Insights/Insights';
 import { TrustUs } from '@/src/components/Main/TrustUs/TrustUs';
 import { FeedbackDataExtertise } from '@/src/utils/DataLayers/FeedbackData';
 import styles from './expertise.module.css';
+import { Section } from '@/src/components/shared/Section/Section';
+import { Container } from '@/src/components/shared/Container/Container';
 
 export default function Expertise() {
   return (
     <>
-      <Header />
       <main className='mt-[80px] bg-main-bg text-white'>
-        <section id='title' className='pt-[40px]'>
-          <ExpertiseTitle />
-        </section>
-        <section id='items' className='mt-[40px] desktop:mt-[80px]'>
-          <Items />
-        </section>
-        <section id='virtual-cio'>
-          <div className={styles.contentContainer}>
-            <Virtual />
-            <BusinessProcess />
-            <ProjectsRisks />
-            <CostumDevelopment />
-            <SystemIntegration />
-            <ScalableArchitecture />
-            <MobileApplications />
-          </div>
-        </section>
-        <section id='insights' className='mt-[40px]'>
+        <Section id='title'>
+          <Container>
+            <ExpertiseTitle />
+          </Container>
+        </Section>
+        <Section id='items' className='pb-0 tablet:pb-0 desktop:pb-0'>
+          <Container>
+            <Items />
+          </Container>
+        </Section>
+        <Section id='virtual-cio' className='pt-0 tablet:pt-0 desktop:pt-0'>
+          <Container>
+            <div className={styles.contentContainer}>
+              <Virtual />
+              <BusinessProcess />
+              <ProjectsRisks />
+              <CostumDevelopment />
+              <SystemIntegration />
+              <ScalableArchitecture />
+              <MobileApplications />
+            </div>
+          </Container>
+        </Section>
+        <Section id='insights' className='mt-[40px]'>
           <Insights />
-        </section>
-        <section>
-          <TrustUs />
-        </section>
-        <section
+        </Section>
+        <Section>
+          <Container>
+            <TrustUs />
+          </Container>
+        </Section>
+        <Section
           id='feedback'
           className='mt-[40px] tablet:mt-[60px] desktop:mt-[80px]'
         >
-          <FeedbackClient feedback={FeedbackDataExtertise} />
-        </section>
-        <section id='contacts'>
-          <ContactForm />
-        </section>
+          <Container>
+            <FeedbackClient feedback={FeedbackDataExtertise} />
+          </Container>
+        </Section>
+        <Section
+          id='contacts'
+          light
+          className='py-[40px] tablet:py-[80px] desktop:py-[80px]'
+        >
+          <Container>
+            <ContactForm />
+          </Container>
+        </Section>
       </main>
-      <Footer />
     </>
   );
 }
