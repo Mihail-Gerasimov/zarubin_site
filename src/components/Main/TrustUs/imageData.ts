dfb944b245ee82d08f04fe19a58732874fc2bed2
Arsalan Rabdanov <uzarsalan@gmail.com>
2024-06-25 00:18:26 +0800

    fixed client logos

--- src/components/Main/TrustUs/imageData.ts
+++ src/components/Main/TrustUs/imageData.ts
@@ -1,12 +1,13 @@
-import gazpromImg from '@/public/assets/images/main/gazprom.png';
-import avangardImg from '@/public/assets/images/main/hk_avangard.png';
-import traktorImg from '@/public/assets/images/main/hk_traktor.png';
+import avangard from '@/public/assets/images/clients/avangard.png';
+import dinamo from '@/public/assets/images/clients/dinamo.png';
+import kama from '@/public/assets/images/clients/kama.png';
+import nils from '@/public/assets/images/clients/nils.png';
+import orbita from '@/public/assets/images/clients/orbit.png';
 
 export const imageData = [
-  { id: 1, image: gazpromImg, alt: 'gazprom logo' },
-  { id: 2, image: avangardImg, alt: 'hk avangard' },
-  { id: 3, image: traktorImg, alt: 'hk traktor' },
-  { id: 4, image: gazpromImg, alt: 'gazprom logo' },
-  { id: 5, image: avangardImg, alt: 'hk avangard' },
-  { id: 6, image: traktorImg, alt: 'hk traktor' },
+  { id: 1, image: avangard, alt: 'avangard' },
+  { id: 2, image: dinamo, alt: 'dinamo' },
+  { id: 3, image: kama, alt: 'kama' },
+  { id: 4, image: nils, alt: 'nils' },
+  { id: 5, image: orbita, alt: 'orbita' },
 ];
