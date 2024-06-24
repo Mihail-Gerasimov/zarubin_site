3a2e83edb02cbd88bef9868ac0781e2ba929ed38
Arsalan Rabdanov <uzarsalan@gmail.com>
2024-06-24 23:56:45 +0800

    header hover active styles

--- src/components/NavList/MainList.tsx
+++ src/components/NavList/MainList.tsx
@@ -1,4 +1,6 @@
+import classNames from 'classnames';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface Props {
   list: List[];
@@ -12,13 +14,21 @@ interface List {
 }
 
 export const MainList = ({ list, dark = true }: Props) => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <ul className='hidden justify-center gap-[44px] desktop:flex'>
       {list.map((item) => (
         <li key={item.id}>
           <Link
             href={item.link}
-            className={`font-proxima leading-[1.87] ${dark ? 'text-[26px] text-white' : 'text-[16px] text-[black]'}`}
+            className={classNames(
+              `border-solid border-main-blue font-proxima leading-[1.87] hover:border-b-[2px]`,
+              dark ? 'text-[26px] text-white' : 'text-[16px] text-[black]',
+              {
+                'border-b-[2px]': pathname.startsWith(item.link),
+              },
+            )}
           >
             {item.name}
           </Link>
