99ed0addeb1b72250ea02ea9ebce282dafc39dfe
ALFAT GILMANSHIN <89654590+Alfat1989@users.noreply.github.com>
2024-08-16 14:08:55 +0500

    change header adaptive (#109)
    * fix: change header adaptive
    
    * fix: nav menu size

--- src/components/NavList/MainList.tsx
+++ src/components/NavList/MainList.tsx
@@ -16,14 +16,16 @@ interface List {
 export const MainList = ({ list, dark = true }: Props) => {
   const pathname = usePathname();
   return (
-    <ul className='hidden justify-center gap-[44px] desktop:flex'>
+    <ul className='hidden justify-center gap-[44px] laptop-big:flex'>
       {list.map((item) => (
         <li key={item.id}>
           <Link
             href={item.link}
             className={classNames(
               `border-solid border-main-blue font-proxima leading-[1.87] hover:border-b-[2px]`,
-              dark ? 'text-[26px] text-white' : 'text-[16px] text-[black]',
+              dark
+                ? 'text-[22px] text-white desktop:text-[26px]'
+                : 'text-[16px] text-[black]',
               {
                 'border-b-[2px]': pathname.startsWith(item.link),
               },
--- src/ui-kit/LeagueLink/ContactUsBtn.tsx
+++ src/ui-kit/LeagueLink/ContactUsBtn.tsx
@@ -8,7 +8,7 @@ export const ContactUsBtn = ({ dark }: { dark: boolean }) => {
       href='#contacts'
       className={twMerge(
         classNames(
-          'font-proxima-reg hidden h-[46px] min-w-[162px] items-center justify-center rounded-[4px] font-proxima text-[20px] font-bold transition tablet:rounded-[8px] desktop:flex desktop:rounded-[6px]',
+          'font-proxima-reg hidden h-[46px] min-w-[162px] items-center justify-center rounded-[4px] font-proxima text-[20px] font-bold transition tablet:rounded-[8px] laptop-big:flex desktop:rounded-[6px]',
           dark
             ? 'border-2 border-solid border-emerald-300 hover:border-[#7CFFCD] hover:bg-[#7CFFCD] hover:text-text-dark'
             : 'bg-main-blue text-white hover:bg-main-blue-hover',
