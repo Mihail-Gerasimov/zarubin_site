789a8436470203413a5fbe97e3379357b3c80bc9
ALFAT GILMANSHIN <89654590+Alfat1989@users.noreply.github.com>
2024-08-22 17:31:31 +0500

    feat: add sitemap config (#112)

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteUrl: 'https://www.zarubin.co.uk/',
+    generateSitemap: true,
+    generateIndexSitemap: true,
+    generateRobotsTxt: true,
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+}
