7bef44c357a43d656f6d3b273592e112ee13d6e6
alfat <gilmalf89@gmail.com>
2024-09-13 19:22:21 +0500

    fix: add new feedback

--- src/utils/DataLayers/FeedbackData.ts
+++ src/utils/DataLayers/FeedbackData.ts
@@ -2,8 +2,11 @@ import anotherDmitry from '@/public/assets/images/expertise/another_Dmitry.webp'
 import alexeyPhoto from '@/public/assets/images/main/feedbacks/alexey.webp';
 import dmitryPhoto from '@/public/assets/images/main/feedbacks/dmitriy.webp';
 import elenaPhoto from '@/public/assets/images/main/feedbacks/elena.webp';
+import johnPhoto from '@/public/assets/images/main/feedbacks/john.webp';
 import konstantinPhoto from '@/public/assets/images/main/feedbacks/konstantin.webp';
 import sergeyPhoto from '@/public/assets/images/main/feedbacks/sergey.webp';
+import vasiliyPhoto from '@/public/assets/images/main/feedbacks/vasiliy.webp';
+import vitaliyPhoto from '@/public/assets/images/main/feedbacks/vitaliy.webp';
 
 export const FeedbackDataMain = [
   {
@@ -45,6 +48,30 @@ export const FeedbackDataMain = [
     image: konstantinPhoto,
     feedback:
       'Vitaly always takes a proactive position, he really helped the company to move from offline to online. He brought expertise from other industries to the sport and perfected it!',
+  },
+  {
+    id: 6,
+    name: 'Vasiliy',
+    job: 'CEO IGFT',
+    image: vasiliyPhoto,
+    feedback:
+      "We have been working with Vitaliy for two years, during which time we have actively developed our project. The team is always results-driven and takes a proactive approach, anticipating potential challenges and addressing them promptly. Since we work with a complex IT system, it's crucial for us that all components are perfectly fine-tuned. Zarubin & Co impresses with their attention to detail: the team provides comprehensive API documentation, every new feature is meticulously described in a dedicated document, and they constantly offer suggestions for improvement. At the same time, they help us avoid unnecessary actions if our plans go beyond what is required.",
+  },
+  {
+    id: 7,
+    name: 'John Cushma',
+    job: 'Entrepreneur, 21sports',
+    image: johnPhoto,
+    feedback:
+      "We are working on a sports publication website with a relatively simple tech stack that involves integrations. Previous developers consistently delivered an incomplete product full of bugs and were unresponsive when issues were pointed out. Vitaliy's team has shown a high level of professionalism, adhering to a clear business process and a systematic approach at every stage. I highly recommend working with them.",
+  },
+  {
+    id: 8,
+    name: 'Vitaliy',
+    job: 'CTO, Hockey Club Avangard',
+    image: vitaliyPhoto,
+    feedback:
+      'We are working on the hockey club project, focusing on design and support. I am fully confident that when I hand over my project to Vitaliy, the result is guaranteed. He and his team are genuinely invested and bring their own perspective to the work. Unlike companies that require a detailed technical specification, Vitaliy and his team are more comfortable independently leading the project and defining its boundaries.',
   },
 ];
 
