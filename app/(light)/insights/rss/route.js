ccc8db8d8375bdaaa9e35a136eaaccdada809794
ALFAT GILMANSHIN <89654590+Alfat1989@users.noreply.github.com>
2024-10-28 23:35:06 +0500

    fix: change feed response (#181)

--- app/(light)/insights/rss/route.js
+++ app/(light)/insights/rss/route.js
@@ -1,9 +1,8 @@
-import { getPostMetadata } from '@/src/utils/getPostMetadata';
-import { NextResponse } from 'next/server';
-import { DateTime } from 'luxon';
-import RSS from 'rss';
+import { getPostMetadata } from '@/src/utils/getPostMetadata'
+import { DateTime } from 'luxon'
+import RSS from 'rss'
 
-const allInsights = getPostMetadata('src/posts');
+const allInsights = getPostMetadata('src/posts')
 
 export async function GET() {
   const feed = new RSS({
@@ -14,22 +13,22 @@ export async function GET() {
     copyright: `${new Date().getFullYear()} Bright Byte Insights`,
     language: 'en-us',
     pubDate: new Date().toUTCString(),
-  });
+  })
 
   allInsights.forEach((insight) => {
     const formattedDate = insight.date
       ? DateTime.fromFormat(insight.date, 'yyyy-MM-dd').toRFC2822()
-      : null;
+      : null
     feed.item({
       title: String(insight.title),
       description: String(insight.description),
       guid: `https://thebrightbyte.com/insights/${insight.slug}`,
       url: `https://thebrightbyte.com/insights/${insight.slug}`,
       date: formattedDate,
-    });
-  });
+    })
+  })
 
-  return new NextResponse(feed.xml({ indent: true }), {
+  return new Response(feed.xml({ indent: true }), {
     headers: { 'Content-Type': 'application/rss+xml; charset=utf-8' },
-  });
+  })
 }
