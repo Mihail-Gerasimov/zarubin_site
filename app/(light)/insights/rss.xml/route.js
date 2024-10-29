eea52eb5c1e9f2b3066d919620b85203aeadb6d2
ALFAT GILMANSHIN <89654590+Alfat1989@users.noreply.github.com>
2024-10-29 17:09:41 +0500

    fix: check insights rss page error (#189)

--- app/(light)/insights/rss.xml/route.js
+++ app/(light)/insights/rss.xml/route.js
@@ -1,34 +1,34 @@
-import { getPostMetadata } from '@/src/utils/getPostMetadata';
-import { DateTime } from 'luxon';
-import RSS from 'rss';
+import { getPostMetadata } from '@/src/utils/getPostMetadata'
+import { DateTime } from 'luxon'
+import RSS from 'rss'
 
-const allInsights = getPostMetadata('src/posts');
+const allInsights = getPostMetadata('src/posts')
 
 export async function GET() {
   const feed = new RSS({
     title: 'Bright Byte Insights',
     description: 'Latest insights from Bright Byte',
     site_url: 'https://thebrightbyte.com',
-    feed_url: `https://thebrightbyte.com/insights/rss`,
+    feed_url: `https://thebrightbyte.com/insights/rss.xml`,
     copyright: `${new Date().getFullYear()} Bright Byte Insights`,
     language: 'en-us',
     pubDate: new Date().toUTCString(),
-  });
+  })
 
   allInsights.forEach((insight) => {
     const formattedDate = insight.date
       ? DateTime.fromFormat(insight.date, 'yyyy-MM-dd').toRFC2822()
-      : null;
+      : null
     feed.item({
       title: String(insight.title),
       description: String(insight.description),
       guid: `https://thebrightbyte.com/insights/${insight.slug}`,
       url: `https://thebrightbyte.com/insights/${insight.slug}`,
       date: formattedDate,
-    });
-  });
+    })
+  })
 
-  return new Response(feed, {
+  return new Response(feed.xml({ indent: true }), {
     headers: { 'Content-Type': 'application/rss+xml; charset=utf-8' },
-  });
+  })
 }
