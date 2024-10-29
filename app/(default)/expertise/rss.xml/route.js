e9b4ee485409d94d1d3ad90058b4f35b22b7ca5d
alfat <gilmalf89@gmail.com>
2024-10-29 16:53:51 +0500

    fix: correct insights rss file

--- app/(default)/expertise/rss.xml/route.js
+++ app/(default)/expertise/rss.xml/route.js
@@ -1,8 +1,8 @@
-import { getExpertiseMetadata } from '@/src/utils/getExpertiseMetadata'
-import { DateTime } from 'luxon'
-import RSS from 'rss'
+import { getExpertiseMetadata } from '@/src/utils/getExpertiseMetadata';
+import { DateTime } from 'luxon';
+import RSS from 'rss';
 
-const allExpertise = getExpertiseMetadata('src/expertise')
+const allExpertise = getExpertiseMetadata('src/expertise');
 
 export async function GET() {
   const feed = new RSS({
@@ -13,22 +13,22 @@ export async function GET() {
     copyright: `${new Date().getFullYear()} Bright Byte Expertise`,
     language: 'en-us',
     pubDate: new Date().toUTCString(),
-  })
+  });
 
   allExpertise.forEach((expertise) => {
     const formattedDate = expertise.date
       ? DateTime.fromFormat(expertise.date, 'yyyy-MM-dd').toRFC2822()
-      : null
+      : null;
     feed.item({
       title: String(expertise.title),
       description: String(expertise.description),
       guid: `https://thebrightbyte.com/expertise/${expertise.slug}`,
       url: `https://thebrightbyte.com/expertise/${expertise.slug}`,
       date: formattedDate,
-    })
-  })
+    });
+  });
 
   return new Response(feed.xml({ indent: true }), {
-    headers: { 'Content-Type': 'application/atom+xml; charset=utf-8' },
-  })
+    headers: { 'Content-Type': 'application/rss+xml; charset=utf-8' },
+  });
 }
