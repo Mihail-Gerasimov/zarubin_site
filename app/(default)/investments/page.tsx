4f91bff063dbd2d079c545237e7a5af0e7d0c0cb
ALFAT GILMANSHIN <89654590+Alfat1989@users.noreply.github.com>
2024-11-11 14:08:55 +0500

    feat: add investments navigation link (#202)

--- app/(default)/investments/page.tsx
+++ app/(default)/investments/page.tsx
@@ -2,9 +2,39 @@ import { ContactForm } from '@/src/components/Main/ContactForm/ContactForm';
 import { Products } from '@/src/components/NewProducts/Products/Products';
 import { Container } from '@/src/components/shared/Container/Container';
 import { Section } from '@/src/components/shared/Section/Section';
+import { BASE_URL } from '@/src/utils/alias';
 import { getProductsMetadata } from '@/src/utils/getProductsMetadata';
+import { openGraphImage } from '@/src/utils/openGraphParams';
+import { pageMetadata } from '@/src/utils/pageMetadata';
+import { Metadata } from 'next';
 
 const products = getProductsMetadata('src/products');
+
+const title = pageMetadata.investments.title;
+const description = pageMetadata.investments.title;
+const keywords = pageMetadata.investments.keywords;
+
+export const metadata: Metadata = {
+  title,
+  description,
+  metadataBase: new URL(BASE_URL),
+  icons: {
+    icon: '/assets/images/info/main_meta.png',
+  },
+  alternates: {
+    canonical: new URL(`${BASE_URL}/our_products`),
+  },
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    siteName: 'BrightByte.com',
+    ...openGraphImage,
+    title,
+    description,
+    url: `${BASE_URL}/our_products`,
+  },
+  keywords,
+};
 
 export default function NewProductsPage() {
   return (
