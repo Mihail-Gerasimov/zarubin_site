2131dbd5f1c6c777d74770fa45477e2e4d96a712
ALFAT GILMANSHIN <89654590+Alfat1989@users.noreply.github.com>
2024-11-14 19:58:22 +0500

    [Des-114] change header (#205)
    * fix: change insights header
    
    * fix: delete instruction page

--- src/ui-kit/LeagueLink/ContactUsBtn.tsx
+++ src/ui-kit/LeagueLink/ContactUsBtn.tsx
@@ -10,7 +10,7 @@ export const ContactUsBtn = ({ dark }: { dark: boolean }) => {
         classNames(
           'font-proxima-reg hidden h-[46px] min-w-[162px] items-center justify-center rounded-[4px] font-proxima text-[20px] font-bold transition tablet:rounded-[8px] laptop-big:flex desktop:rounded-[6px]',
           dark
-            ? 'border-2 border-solid border-emerald-300 hover:border-[#7CFFCD] hover:bg-[#7CFFCD] hover:text-text-dark'
+            ? 'border-2 border-solid border-emerald-300 text-white hover:border-[#7CFFCD] hover:bg-[#7CFFCD] hover:text-text-dark'
             : 'bg-main-blue text-white hover:bg-main-blue-hover',
         ),
       )}
