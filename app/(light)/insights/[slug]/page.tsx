0d7ba197bc06c839aeff90400d71ef9438ecfb1a
Vladilen <vladilen597@gmail.com>
2024-11-14 12:33:37 +0300

    fix: make expertise to insights style

--- app/(light)/insights/[slug]/page.tsx
+++ app/(light)/insights/[slug]/page.tsx
@@ -157,7 +157,7 @@ export default function MainBlogSlug(props: { params: { slug: string } }) {
       <GoBackLink />
       <div className='mx-[auto] max-w-[896px] pb-[30px]'>
         <div className='relative flex w-full items-center justify-center'></div>
-        <div className='mt-[200px]'>
+        <div className='mt-[100px]'>
           {readingTime && (
             <span className='mb-[10px] block font-proxima text-[16px] leading-[1.25] text-text-dark opacity-[50%]'>
               Reading time: {readingTime}
